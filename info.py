a00412fce494388a05ceef478dba9752612b25f5
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-08-23 15:31:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '12618934'))
 API_HASH = environ.get('API_HASH', '49aacd0bc2f8924add29fb02e20c8a16')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6530884983:AAE9LlUEAf_lsKgfvN24hM3TW9VVpC5WbLk")
+BOT_TOKEN = environ.get('BOT_TOKEN', "6724470726:AAEqA0FQME2Bh7DVN2dJhpQdwOM3iMEA7l0")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
