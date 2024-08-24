13d90bdf52aa6114c5d0111f7bf4f59786362acd
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-08-24 08:01:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,6 +26,7 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/451f038b4e7c2ddd
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/5e2d4418525832bc9a1b9.jpg")
 
 # Admins, Channels & Users
+OWNER_ID = environ.get('OWNER_ID', '1297128957 1509123054')
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1297128957 1509123054').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001911016310 -1002096796235').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
